c781d7a6fd479919f964ea80e16638add5504851
Yasiru Jayatissa <110222524+yasirujayatissa@users.noreply.github.com>
2022-09-06 12:33:24 +0530

    Add files via upload

---
+++ Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  TravelBuddy
+//
+//  Created by D.Yasiru Jayatissa on 2022-09-06.
+//
+
+import Foundation
+
+struct Constants{
+    
+    struct Stroryboard{
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+}
---
+++ Helpers/Utilities.swift
@@ -0,0 +1,20 @@
+//
+//  Utilities.swift
+//  customauth
+//
+//  Created by Christopher Ching on 2019-05-09.
+//  Copyright © 2019 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+}
